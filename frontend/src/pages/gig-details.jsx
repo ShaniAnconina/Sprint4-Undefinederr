77fd234b1ccaf7abe72080ceed1ac97896ed0fb2
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-17 23:34:30 +0200

    fixed src

--- frontend/src/pages/gig-details.jsx
+++ frontend/src/pages/gig-details.jsx
@@ -3,15 +3,15 @@ import { useParams } from "react-router-dom"
 export function GigDetails() {
     const { gigId } = useParams()
 
+    //TODO:navlink breadcrumds
+    //TODO:mini user: photo, name, miniInfo(level..) , rate(stars+ reviewsCount) , langs...
+    //TODO:photos gallery (carosela) + mini photos
+    //TODO: carosela mini-review (drag-drop support)
+    //TODO: about the gig
+    //TODO: about the seller (img, name, mini-des, rate, contact chat link)
+    //TODO: about the seller div border()
     return (
         <section className="gig-details">
-            //TODO:navlink breadcrumds
-            //TODO:mini user: photo, name, miniInfo(level..) , rate(stars+ reviewsCount) , langs...
-            //TODO:photos gallery (carosela) + mini photos
-            //TODO: carosela mini-review (drag-drop support)
-            //TODO: about the gig
-            //TODO: about the seller (img, name, mini-des, rate, contact chat link)
-            //TODO: about the seller div border()
 
         </section>
     )
