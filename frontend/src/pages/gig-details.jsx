dc5a857bcf3174441087b380c60bda3e34900a54
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-18 00:43:03 +0200

    edit the details page

--- frontend/src/pages/gig-details.jsx
+++ frontend/src/pages/gig-details.jsx
@@ -1,17 +1,52 @@
 import { useParams } from "react-router-dom"
+import { useRef } from "react"
+
 
 export function GigDetails() {
     const { gigId } = useParams()
+    const elReview = useRef(null)
 
-    //TODO:navlink breadcrumds
-    //TODO:mini user: photo, name, miniInfo(level..) , rate(stars+ reviewsCount) , langs...
-    //TODO:photos gallery (carosela) + mini photos
-    //TODO: carosela mini-review (drag-drop support)
-    //TODO: about the gig
-    //TODO: about the seller (img, name, mini-des, rate, contact chat link)
-    //TODO: about the seller div border()
+    function goToReview() {
+        const revewPlace = elReview.current.scrollIntoView({ behavior: "smooth" })
+        // revewPlace.scrollIntoView({ behavior: "smooth" })
+    }
+
     return (
         <section className="gig-details">
+        //TODO:navlink breadcrumds
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+        //TODO:mini user: photo, name, miniInfo(level..) , rate(stars+ reviewsCount) , langs...
+            <div className="mini-user"> Lorem ipsum dolor sit amet.</div>
+        //TODO:photos gallery (carosela) + mini photos
+
+            //TODO: carosela mini-review (drag-drop support)
+            <br />
+            <h2>What people loved about this seller</h2>
+            <button className='open-btn' onClick={goToReview}>See all reviews</button>
+
+        //TODO: about the gig
+            <h2>About This Gig</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem.</p>
+
+            <strong>What you will get:</strong>
+            
+            //TODO: about the seller (img, name, mini-des, rate, contact chat link)
+            //TODO: about the seller div border()
+            //TODO: packeges 
+            //TODO: FAQ
+            //TODO: full reviews (load more...)
+            //TODO: count of reviews, 'reviews of this gig', rate(stars)
+            //TODO: filter reviews by stars (count)
+            //TODO: search reviews
+            //TODO: sort reviews
+            //TODO: filter, checkBox (reviews with imgs)
+            //TODO: photo, name || country and name || stars , date || body || review for the review(helpful?yes/no)
+
+
+            <div ref={elReview} id='reviews' >reviews!!!!!!!!!!!!!!</div>
 
         </section>
     )
