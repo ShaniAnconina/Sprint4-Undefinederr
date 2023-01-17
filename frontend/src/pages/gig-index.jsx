97ef52e00d9a56f07ba25802adb7ed0c54a76f56
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-17 21:48:21 +0200

    first commit yovel test

--- frontend/src/pages/gig-index.jsx
+++ frontend/src/pages/gig-index.jsx
@@ -2,7 +2,7 @@ export function GigIndex(){
 
     return (
         <section className="gig-index">
-            <h1>hello from gig index</h1>
+            <h1>hello from gig index yovel !</h1>
         </section>
     )
 }
