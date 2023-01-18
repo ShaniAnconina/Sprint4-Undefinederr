75b8998a2dc2eb0907c258a585fbb9911bcc5faa
Shani Anconina <shanianconina310@gmail.com>
2023-01-18 14:49:24 +0200

    add link to details

--- frontend/src/cmps/gig-list.jsx
+++ frontend/src/cmps/gig-list.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GigPreview } from "./gig-preview"
 import { FaHeart } from "react-icons/fa"
 
 export function GigList({ gigs }) {
-    const navigate = useNavigate()
 
     return (
         <ul className="gig-list">
             {gigs.map(gig =>
-                <li className="card" key={gig._id} onClick={navigate(`/gig/${gig._id}`)}>
-                    <GigPreview gig={gig} />
-                    <div className="card-footer">
-                        <p className="save"><FaHeart /></p>
-                        <div className="price">
-                            <p>STARTING AT</p>
-                            <span>₪{gig.price}</span>
+                <li className="card" key={gig._id}>
+                    <Link to={`/gig/${gig._id}`}>
+                        <GigPreview gig={gig} />
+                        <div className="card-footer">
+                            <p className="save"><FaHeart /></p>
+                            <div className="price">
+                                <p>STARTING AT</p>
+                                <span>₪{gig.price}</span>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </li>)}
-        </ul>
+        </ul >
     )
 }
