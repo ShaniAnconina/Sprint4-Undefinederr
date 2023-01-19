24a3984ac35493c9928b501ec42a8e506a1dc819
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-19 20:49:48 +0200

    fixed bugs

--- frontend/src/cmps/owner-rate.jsx
+++ frontend/src/cmps/owner-rate.jsx
@@ -1,12 +1,13 @@
 import { AiFillStar } from "react-icons/ai"
 
-export function OwnerRate({rate}) {
+export function OwnerRate({ rate }) {
     return <>
         {rate >= 1 && <p><AiFillStar size="15px" /></p>}
         {rate >= 2 && <p><AiFillStar size="15px" /></p>}
         {rate >= 3 && <p><AiFillStar size="15px" /></p>}
         {rate >= 4 && <p><AiFillStar size="15px" /></p>}
         {rate >= 5 && <p><AiFillStar size="15px" /></p>}
-
-        {rate} (37)</>
+        <p>{rate}</p>
+        <span>(37)</span>
+    </>
 }
