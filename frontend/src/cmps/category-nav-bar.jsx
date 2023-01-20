22bffa3e40c213d429cb4905bece9fbeb7de479d
Shani Anconina <shanianconina310@gmail.com>
2023-01-20 12:46:00 +0200

    added scroll behave to header on home page

--- frontend/src/cmps/category-nav-bar.jsx
+++ frontend/src/cmps/category-nav-bar.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
 import { getCategories } from '../services/categories-data.js'
 
 export function CategoryNav() {
     const categories = getCategories()
     const titles = []
+    const [scroll, setScroll] = useState(false)
+    const changeScroll = () => {
+        if (window.scrollY > 80) setScroll(true)
+        else setScroll(false)
+    }
+    window.addEventListener('scroll', changeScroll)
 
     for (let key in categories) {
         titles.push(key)
@@ -13,7 +20,8 @@ export function CategoryNav() {
     }
 
     return (
-        <ul className="categories-navbar">
+        <ul className={(!scroll && window.location.hash === '#/') ? 'categories-navbar before-scroll-hide' : 'categories-navbar'}>
+        {/* <ul className="categories-navbar"> */}
             {titles.map((title) => <li key={title}><a onClick={() => { OnSelectCatogery(title) }}>{title}</a></li>)}
         </ul>
     )
