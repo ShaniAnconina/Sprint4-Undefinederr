aec3d74f316cd94b397c07e916a6a8a86e81f8ef
Eliasi1 <eliasiohad@gmail.com>
2023-01-20 12:45:13 +0200

    pushpull

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,11 +1,13 @@
 import {HomeHero} from '../cmps/home-hero.jsx'
 import {AppHeader} from '../cmps/app-header.jsx'
+import {HomePopularServices} from '../cmps/home-popular-services.jsx'
 
 export function HomePage(){
 
     return (
         <section className="home-page">
             <HomeHero/>
+            <HomePopularServices/>
         </section>
     )
 }
