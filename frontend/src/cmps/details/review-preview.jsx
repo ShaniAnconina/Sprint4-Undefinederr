247779ae0a4c5a79898900771c2f72d20745e59e
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-23 12:00:06 +0200

    fixed bugs

--- frontend/src/cmps/details/review-preview.jsx
+++ frontend/src/cmps/details/review-preview.jsx
@@ -14,7 +14,7 @@ export function ReviewPreview({ review }) {
             <span className="review-rate flex align-center" >
                 <span className="flex"><ReviewRate rate={review.rate} />
                 </span>
-                <span>|</span>
+                <p className="separator">|</p>
                 <span>2 weeks ago</span>
             </span>
 
