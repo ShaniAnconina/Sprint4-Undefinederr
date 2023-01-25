6982c0dab76be099be796bd466f98429a7d24a77
Eliasi1 <eliasiohad@gmail.com>
2023-01-25 22:15:44 +0200

    regular push

--- frontend/src/cmps/home/trusted-by.jsx
+++ frontend/src/cmps/home/trusted-by.jsx
@@ -1,5 +1,7 @@
 export function TrustedBy() {
-    return <section className='trusted-by flex align-center justify-center full'>
+    return <section className='trusted-by  main-layout'>
+        <div className="trusted-by-wrap full main-layout">
+            <div className="flex align-center justify-center">
         <span className="trusted-by-text">Trusted by:</span>
         <ul className="trusted-by-logos flex">
             <li><img src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook.31d5f92.png' /></li>
@@ -8,5 +10,7 @@ export function TrustedBy() {
             <li><img src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/pandg.8b7310b.png' /></li>
             <li><img src='https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal.ec56157.png' /></li>
         </ul>
+        </div>
+        </div>
     </section>
 }
