0437cae17114632b0272392cf26cd8e906bebbc0
Eliasi1 <eliasiohad@gmail.com>
2023-01-27 11:14:38 +0200

    working on home page

--- frontend/src/cmps/explore/services-options-modal.jsx
+++ frontend/src/cmps/explore/services-options-modal.jsx
@@ -1,53 +1,57 @@
-export function ServicesOptionsModal({onSubmit}) {
+export function ServicesOptionsModal({ onSubmit }) {
 
-    const titles = ['Graphics & Design','Digital Marketing','Writing & Translation','Video & Animation','Music & Audio','Programming & Tech','Business','Lifestyle','Trending']
+    const titles = ['Graphics & Design', 'Digital Marketing', 'Writing & Translation', 'Video & Animation', 'Music & Audio', 'Programming & Tech', 'Business', 'Lifestyle', 'Trending']
 
     const filterByCategories = []
 
-    function onCheck(value){
-
+    function onChange(value) {
+        let idx = filterByCategories.indexOf(value)
+        if (idx > -1) { // only splice array when item is found
+            filterByCategories.splice(idx, 1)
+        } else filterByCategories.push(value)
+    console.log(filterByCategories)
     }
 
     return (
-        <form id="filters" className="services-options" onSubmit={(ev)=>onSubmit(ev)}>
+        <form id="filters" name="tags" className="services-options" onSubmit={(ev) => onSubmit(ev, filterByCategories)}>
             <div className="service">
-                <input type="checkbox" id="graphics-design" name="graphics-design" value="Graphics and Design" />
+                <input type="checkbox" id="graphics-design" name="graphics-design" value="Graphics and Design" onChange={() => onChange("Graphics and Design")} />
                 <label htmlFor="graphics-design">Graphics & Design</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="digital-marketing" name="digital-marketing" value="Digital Marketing" />
+                <input type="checkbox" id="digital-marketing" name="digital-marketing" value="Digital Marketing" onChange={() => onChange("Digital Marketing")}/>
                 <label htmlFor="digital-marketing">Digital Marketing</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="writing-translation" name="writing-translation" value="Writing and Translation" />
+                <input type="checkbox" id="writing-translation" name="writing-translation" value="Writing and Translation" onChange={() => onChange("Writing and Translation")}/>
                 <label htmlFor="writing-translation">Writing & Translation</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="video-animation" name="video-animation" value="Video and Animation" />
+                <input type="checkbox" id="video-animation" name="video-animation" value="Video and Animation" onChange={() => onChange("Video and Animation")}/>
                 <label htmlFor="video-animation">Video & Animation</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="music-audio" name="music-audio" value="Music and Audio" />
+                <input type="checkbox" id="music-audio" name="music-audio" value="Music and Audio" onChange={() => onChange("Music and Audio")}/>
                 <label htmlFor="music-audio">Music & Audio</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="programming-tech" name="programming-tech" value="Programming and Tech" />
+                <input type="checkbox" id="programming-tech" name="programming-tech" value="Programming and Tech" onChange={() => onChange("Programming and Tech")}/>
                 <label htmlFor="programming-tech">Programming & Tech</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="business" name="business" value="Business" />
+                <input type="checkbox" id="business" name="business" value="Business" onChange={() => onChange("Business")}/>
                 <label htmlFor="business">Business</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="lifestyle" name="lifestyle" value="lifestyle" />
+                <input type="checkbox" id="lifestyle" name="lifestyle" value="lifestyle" onChange={() => onChange("lifestyle")}/>
                 <label htmlFor="lifestyle">Lifestyle</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="trending" name="trending" value="Trending" />
+                <input type="checkbox" id="trending" name="trending" value="Trending" onChange={() => onChange("Trending")}/>
                 <label htmlFor="trending">Trending</label>
             </div>
             <div className="service">
-                <input type="checkbox" id="lifestyle" name="lifestyle" value="Lifestyle" />
+                <input type="checkbox" id="lifestyle" name="lifestyle" value="Lifestyle" onChange={() => onChange("Lifestyle")}/>
                 <label htmlFor="lifestyle">Lifestyle</label>
             </div>
 
