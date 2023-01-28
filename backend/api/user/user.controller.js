30a70bfdb3f33299a8a53c0f7df06eb51147bfce
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-28 17:34:44 +0200

    a

--- backend/api/user/user.controller.js
+++ backend/api/user/user.controller.js
@@ -3,9 +3,10 @@ const socketService = require('../../services/socket.service')
 const logger = require('../../services/logger.service')
 
 async function getUser(req, res) {
+    console.log('here');
     try {
         const user = await userService.getById(req.params.id)
-        res.send(user)
+        res.send(user) 
     } catch (err) {
         logger.error('Failed to get user', err)
         res.status(500).send({ err: 'Failed to get user' })
