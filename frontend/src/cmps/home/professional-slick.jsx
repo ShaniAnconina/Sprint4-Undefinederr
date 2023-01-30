14e30f2beca8f251ffbbe0210d2240d8ccfadf59
Eliasi1 <eliasiohad@gmail.com>
2023-01-30 12:24:33 +0200

    regular commit

--- frontend/src/cmps/home/professional-slick.jsx
+++ frontend/src/cmps/home/professional-slick.jsx
@@ -32,18 +32,18 @@ export class MultipleItems extends Component {
             <div className='categories-list-wrap main-layout'>
                 <Slider ref={c => (this.slider = c)} {...settings}>
                     {/* <Slider {...settings}> */}
-                    <div>
+                    {/* <div> */}
 
                         <div className="img-container">
                             <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/f27bec553efc12cc60baed89b8f2223e-1674661140708/ai-artists-2x.png" alt="" />
                             <p>Add talent to AI<br /><span>AI Artists</span></p>
                         </div>
-                    </div>
+                    {/* </div> */}
                     <div className="img-container">
                         <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_305,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png" alt="" />
                         <p>Build your brand<br /><span>Logo Design</span></p>
                     </div>
-                    <div className="img-container">
+                    <div className="img-container" >
                         <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_305,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png" alt="" />
                         <p>Customize your site<br /><span>WordPress</span></p>
                     </div>
